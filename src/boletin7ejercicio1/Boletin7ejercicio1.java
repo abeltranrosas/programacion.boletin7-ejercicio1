162623cbd106b27c89f489b02ee9664b36d4cb70
antonio <antonio@192.168.0.20>
2015-11-02 20:51:26 +0100

    boletin7ejercicio1

---
+++ src/boletin7ejercicio1/Boletin7ejercicio1.java
@@ -0,0 +1,20 @@
+
+package boletin7ejercicio1;
+
+/**
+ *
+ * @author antonio
+ */
+public class Boletin7ejercicio1 {
+
+   
+    public static void main(String[] args) {
+        int num;{
+             System.out.print("numero + es positivo");
+        
+    }
+        
+        }
+    
+
+    }
